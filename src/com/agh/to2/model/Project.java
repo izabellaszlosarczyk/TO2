9b435893118da0fa430ec45d01ce67aa7fd595ea
izabellaszlosarczyk <iza.bella.szlosarczyk@gmail.com>
2015-11-30 17:41:48 +0100

    formatowanie

--- src/com/agh/to2/model/Project.java
+++ src/com/agh/to2/model/Project.java
@@ -9,26 +9,31 @@ public class Project implements IProject {
 	private Date deadline;
 	private Date startdate;
 	private ITeam team;
-	
-	public Project(){
+
+	public Project() {
 		this.id = UUID.randomUUID().toString();
 	}
-	
+
 	public String getId() {
 		return id;
 	}
+
 	public void setId(String id) {
 		this.id = id;
 	}
+
 	public Date getDeadline() {
 		return deadline;
 	}
+
 	public void setDeadline(Date deadline) {
 		this.deadline = deadline;
 	}
+
 	public Date getStartdate() {
 		return startdate;
 	}
+
 	public void setStartdate(Date startdate) {
 		this.startdate = startdate;
 	}
