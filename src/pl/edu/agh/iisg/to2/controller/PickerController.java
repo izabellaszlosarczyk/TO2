8e9a738d95d7c81fde3e1d2ee512bdfb2e0f3fbf
Michal Smialko <michal.moriquendi@gmail.com>
2015-12-13 11:54:47 +0100

    Add rough implementation of some PickerController's interfaces.

--- src/pl/edu/agh/iisg/to2/controller/PickerController.java
+++ src/pl/edu/agh/iisg/to2/controller/PickerController.java
@@ -1,34 +1,53 @@
 package pl.edu.agh.iisg.to2.controller;
 
+import java.math.BigDecimal;
+
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.stage.Stage;
+import pl.edu.agh.iisg.to2.model.EmployeeMock;
+import pl.edu.agh.iisg.to2.model.IEmployee;
+import pl.edu.agh.iisg.to2.model.ITeam;
 import pl.edu.agh.iisg.to2.model.ProjectMock;
 
 public class PickerController {
 	
-	@FXML private TableView<ProjectMock> pickerTable;
-	@FXML private TableColumn<ProjectMock, String> nameColumn;
-	@FXML private TableColumn<ProjectMock, String> costColumn;
-	@FXML private TableColumn<ProjectMock, String> checkColumn;
+	@FXML private TableView<IEmployee> pickerTable;
+	@FXML private TableColumn<IEmployee, String> nameColumn;
+	@FXML private TableColumn<IEmployee, BigDecimal> costColumn;
+	@FXML private TableColumn<IEmployee, String> checkColumn;
 	
 	@FXML private Button okButton;
-
-
 	private Stage dialogStage;
+	
+	private ObservableList<IEmployee> allEmployees;
+	private ObservableList<IEmployee> selectedEmployees;
 	
 	@FXML
 	private void initialize() {
+//		nameColumn.setCellValueFactory(value -> value.getValue().getFirstName());
+//		costColumn.setCellValueFactory(value -> value.getValue().getSalary());
 	}
 	
 	@FXML
 	private void handleOkAction(ActionEvent event) {
-
+		dialogStage.close();
 	}
 	
+	/// Public Interface
+	
+	public void setData(ObservableList<IEmployee> allEmployees,
+						ObservableList<IEmployee> selectedEmployees) {
+		this.allEmployees = allEmployees;
+		this.selectedEmployees = selectedEmployees;
+		
+		pickerTable.getItems().setAll(allEmployees);
+	}
 	
 	public void setDialogStage(Stage dialogStage) {
 		this.dialogStage = dialogStage;
