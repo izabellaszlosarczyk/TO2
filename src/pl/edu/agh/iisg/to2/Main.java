eeb294f40642461ba04f358cbdc1829168d46232
Michal Smialko <michal.moriquendi@gmail.com>
2015-12-20 11:49:35 +0100

    Add basic code to connect Java App with MySQL Database.

--- src/pl/edu/agh/iisg/to2/Main.java
+++ src/pl/edu/agh/iisg/to2/Main.java
@@ -3,6 +3,7 @@ package pl.edu.agh.iisg.to2;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import pl.edu.agh.iisg.to2.controller.ProjectController;
+import pl.edu.agh.iisg.to2.model.MySQLAccess;
 //import pl.edu.agh.iisg.to.javafx.cw4.controller.AccountAppController;
 
 public class Main extends Application {
@@ -21,8 +22,19 @@ public class Main extends Application {
 	}
 
 	public static void main(String[] args) {
+		
+		System.out.println("Setup SQL...");
+		MySQLAccess dao = new MySQLAccess();
+	    try {
+			dao.readDataBase();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	    
+		
 		System.out.println("Running...");
 		launch(args);
+		
 	}
 	
 }
